5117f25965139fec163e1f87c0347378ab6e747e
pyxis-star <66399408+pyxis-star@users.noreply.github.com>
2020-10-19 07:47:25 -0300

    Create utils.js

---
+++ src/structures/utils.js
@@ -0,0 +1,18 @@
+const Viola = {}
+
+Viola.time = function formatDate(template, date) {
+            var specs = 'YYYY:MM:DD:HH:mm:ss'.split(':')
+            date = new Date(date || Date.now() - new Date().getTimezoneOffset() * 6e4)
+            return date.toISOString().split(/[-:.TZ]/).reduce(function (template, item, i) {
+                return template.split(specs[i]).join(item)
+            }, template)
+        }
+
+Viola.clean = function clean(text) {
+			const blankSpace = String.fromCharCode(8203)
+			return typeof text === 'string' ? text.replace(/`/g, '`' + blankSpace).replace(/@/g, '@' + blankSpace) : text
+		};
+
+Viola.arts = [`[\`シエラ\`](https://www.pixiv.net/en/artworks/74257065)`]
+
+module.exports = Viola
