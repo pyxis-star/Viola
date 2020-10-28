0fecd8876d37073a8dd17c30ed49ebadf33ce281
pyxis-star <66399408+pyxis-star@users.noreply.github.com>
2020-10-28 09:46:35 -0300

    v12.4.1 DJS

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 const Client = require("./src/client");
 const dotenv = require("dotenv");
+const { Intents } = require('discord.js');
 const fs = require("fs");
-const client = new Client();
-
-client.categories = fs.readdirSync('./src/commands');
+const client =  new Client({ ws: { intents: Intents.ALL } });
 
 dotenv.config({
     path: `${__dirname}/.env`
