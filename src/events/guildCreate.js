fca04a00c5c87cbec76b1de50264e5718e47a82e
pyxis-star <66399408+pyxis-star@users.noreply.github.com>
2020-10-30 22:14:14 -0300

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,5 +1,16 @@
 const chalk = require("chalk");
+const mongoose = require('mongoose');
+const config = require('../../config.json');
+const Guild = require('../models/guild');
 
 module.exports = (client, guild) => {
-    console.log(`[ ${chalk.green('GUILD CREATE')} ] - New server!\nOwner: ${guild.owner.user.tag} - Guild: ${guild.name} (${guild.id})`);
+    guild = new Guild({
+      _id: mongoose.Types.ObjectId(),
+      guildID: guild.id,
+      guildName: guild.name
+   });
+    guild.save()
+    .then(result => console.log(result))
+    .catch(err => console.error(err));
+    console.log(`[ ${chalk.green('GUILD CREATE')} ] - New server!\nGuild: ${guild.name} (${guild.id})`);
 };
