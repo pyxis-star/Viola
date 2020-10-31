2bf223f1b6a4d019751fc58ded9c37c7aed104f1
pyxis-star <66399408+pyxis-star@users.noreply.github.com>
2020-10-30 22:02:45 -0300

    Update ReviewCommand.js

--- src/commands/Util/ReviewCommand.js
+++ src/commands/Util/ReviewCommand.js
@@ -19,23 +19,24 @@ module.exports = {
     run: async (client, message, args) => {
         let review = args[0] || message.attachments.first().url
         if (!review) return;
+        if (!review.endsWith('.png')) return message.reply('**~ The image is not a png!**')
         
         async function fn() {
-  			const pic = await axios.get(`${review}`, {
+  		const pic = await axios.get(`${review}`, {
     			responseType: 'arraybuffer',
   		});
-  			const model = await nsfw.load();
-  			const image = await tf.node.decodeImage(pic.data,3);
-  			const predictions = await model.classify(image);
+  		const model = await nsfw.load();
+  		const image = await tf.node.decodeImage(pic.data,3);
+  		const predictions = await model.classify(image);
   
   			image.dispose();
-            const embed = new MessageEmbed()
-               .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-               .setTimestamp()
-               .setColor(config.color)
-               .setDescription(`**Neutral : \`${(predictions.find(a => a.className == "Neutral").probability).toFixed(2).slice(2)}%\`\nDrawing : \`${(predictions.find(a => a.className == "Drawing").probability).toFixed(2).slice(2)}%\`\nSexy : \`${(predictions.find(a => a.className == "Sexy").probability).toFixed(2).slice(2)}%\`\nPorn : \`${(predictions.find(a => a.className == "Porn").probability).toFixed(2).slice(2)}%\`\nHentai : \`${(predictions.find(a => a.className == "Hentai").probability).toFixed(2).slice(2)}%\`**`)
-            message.channel.send(embed)
+            	const embed = new MessageEmbed()
+               		.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+               		.setTimestamp()
+               		.setColor(config.color)
+               		.setDescription(`**Neutral : \`${(predictions.find(a => a.className == "Neutral").probability).toFixed(2).slice(2)}%\`\nDrawing : \`${(predictions.find(a => a.className == "Drawing").probability).toFixed(2).slice(2)}%\`\nSexy : \`${(predictions.find(a => a.className == "Sexy").probability).toFixed(2).slice(2)}%\`\nPorn : \`${(predictions.find(a => a.className == "Porn").probability).toFixed(2).slice(2)}%\`\nHentai : \`${(predictions.find(a => a.className == "Hentai").probability).toFixed(2).slice(2)}%\`**`)
+            	message.channel.send(embed)
 		}
-fn()
+fn().catch(e => message.reply('**~ Sorry, try again...**'))
     },
 };
