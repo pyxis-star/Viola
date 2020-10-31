b622069f6146870ca44a56d293e0e8943983b6c6
pyxis-star <66399408+pyxis-star@users.noreply.github.com>
2020-10-30 22:15:27 -0300

    Update HelpCommand.js

--- src/commands/Info/HelpCommand.js
+++ src/commands/Info/HelpCommand.js
@@ -26,11 +26,13 @@ module.exports = {
             .setThumbnail(client.user.avatarURL())
             .setTimestamp()
             .setColor(config.color)
+            .setFooter(`Shard: ${message.guild.shardID}`)
             .setDescription(`**Default prefix is '\`${config.prefix}\`'!\nDoubt in any command? Use \`${config.prefix}help\` \`command name\`**`)
             .addField('Misc commands', `\`${client.commands.filter(u => u.config.category === 'Misc').map(o => o.config.name).join('`, `')}\``, false)
             .addField('Info commands', `\`${client.commands.filter(u => u.config.category === 'Info').map(o => o.config.name).join('`, `')}\``, false)
             .addField('Fun commands', `\`${client.commands.filter(u => u.config.category === 'Fun').map(o => o.config.name).join('`, `')}\``, false)
             .addField('Util commands', `\`${client.commands.filter(u => u.config.category === 'Util').map(o => o.config.name).join('`, `')}\``, false)
+            .addField('Image Manipulation commands', `\`${client.commands.filter(u => u.config.category === 'ImageManipulation').map(o => o.config.name).join('`, `')}\``);
 
             message.channel.send(embed)
         }
@@ -41,11 +43,12 @@ module.exports = {
             
             	if (!cmd) {
             	return message.channel.send(new MessageEmbed().setColor(config.color).setTimestamp().setDescription(info));    
-			} else {
+		} else {
                 if (cmd.config.name) info = `**Command Name**: \`${cmd.config.name}\``;
-        		if (cmd.config.aliases) info += `\n**Aliases**: ${cmd.config.aliases.map(a => `\`${a}\``).join(', ')}`;
+        	if (cmd.config.aliases) info += `\n**Aliases**: ${cmd.config.aliases.map(a => `\`${a}\``).join(', ')}`;
                 if (cmd.config.cooldown) info += `\n**Cooldown**: \`${cmd.config.cooldown}\``;
-       		 	if (cmd.config.description) info += `\n**Description**: \`${cmd.config.description}\``;
+       		if (cmd.config.description) info += `\n**Description**: \`${cmd.config.description}\``;
+                if (cmd.config.usage) info += `\n**Usage**: \`${cmd.config.usage}\``;
                 if (cmd.config.userPerms) info += `\n**User perms**: ${cmd.config.userPerms}`;
                 if (cmd.config.clientPerms) info += `\n**Bot perms**: ${cmd.config.clientPerms}`;
                 
