a404d6ba3949e985ba6f15d69d0a2a683f46391d
pyxis-star <66399408+pyxis-star@users.noreply.github.com>
2020-10-30 22:13:49 -0300

    Update guildDelete.js

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,5 +1,11 @@
 const chalk = require("chalk");
+const Guild = require("../models/guild");
 
 module.exports = (client, guild) => {
+    Guild.findOneAndDelete({
+        guildID: guild.id
+    }, (err) => {
+        if (err) console.log(err)
     console.log(`[ ${chalk.red('GUILD DELETE')} ] - Guild deleted!`);
+    });
 };
